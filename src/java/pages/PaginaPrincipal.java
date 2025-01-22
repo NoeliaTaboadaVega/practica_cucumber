386eecbc0e9eb439446bf1424a81300d8adf14c9
Noelia Taboada <noeliataboadavega@gmail.com>
2025-01-22 19:28:21 -0300

    Codeo las POMs

---
+++ src/java/pages/PaginaPrincipal.java
@@ -0,0 +1,20 @@
+package java.pages;
+
+import java.util.List;
+
+public class PaginaPrincipal extends BasePage{
+
+    private String categoriasLink = "//a[@class='nav-menu-categories-link']";
+    private String categoriasDropdown = "//ul[@class='nav-categs-departments']";
+
+
+
+    public PaginaPrincipal(){
+        super(driver);
+    }
+    
+    public List<String> returnValuesOfDropdown() {
+        return getDropDownValues(categoriasLink, categoriasDropdown);
+    }
+    
+}
