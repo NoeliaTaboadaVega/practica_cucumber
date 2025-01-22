5939abdbfaf85a92f904156d313dc7f05d93e7e3
Noelia Taboada <noeliataboadavega@gmail.com>
2025-01-22 18:57:40 -0300

    Configuro el archivo TestRunner

--- src/java/runner/TestRunner.java
+++ src/java/runner/TestRunner.java
@@ -1,3 +1,21 @@
+package java.runner;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import pages.BasePage;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features="src/test/resources", //Directorio de nuestros archivos .feature
+    glue="steps", //Paquete donde tenemos nuestras clases definiendo los steps escritos en el feature file
+    plugin={"pretty","html:target/cucumber-reports"} //Donde queremos el reporte
+)
 public class TestRunner {
-    
+    @AfterClass
+    public static void cleanDriver(){
+        BasePage.closeBrowser();
+    }
 }
